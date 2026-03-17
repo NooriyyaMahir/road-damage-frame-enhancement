c7facb475552c6a6b23be98ee4f3322e9a17fc5a
FathiSam16 <samitha2k02@gmail.com>
2026-03-16 17:50:14 -0700

    Update illumination_variation.py

--- illumination_variation.py
+++ illumination_variation.py
@@ -3,9 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-# ----------------------------
+# -----------------------------------
+# Member 2 : Illumination Analysis
+# CLAHE Contrast Enhancement
+# -----------------------------------
+
 # Image Path
-# ----------------------------
 image_path = r"01_original_frames\frame_100.png"
 
 if not os.path.exists(image_path):
@@ -21,26 +24,23 @@ if gray is None:
 
 print("Image loaded successfully.")
 
-# ---------------------------------------
-# Apply CLAHE
-# ---------------------------------------
+# -----------------------------------
+# Apply CLAHE (Contrast Limited Adaptive Histogram Equalization)
+# -----------------------------------
 
-# Create CLAHE object
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-
-# Apply CLAHE
 clahe_image = clahe.apply(gray)
 
-# ---------------------------------------
+# -----------------------------------
 # Save Output
-# ---------------------------------------
+# -----------------------------------
 
 cv2.imwrite("clahe_output.jpg", clahe_image)
 print("CLAHE enhanced image saved.")
 
-# ---------------------------------------
-# Display Comparison
-# ---------------------------------------
+# -----------------------------------
+# Display Image Comparison
+# -----------------------------------
 
 plt.figure(figsize=(12,5))
 
@@ -57,9 +57,9 @@ plt.axis('off')
 plt.tight_layout()
 plt.show()
 
-# ---------------------------------------
+# -----------------------------------
 # Histogram Comparison
-# ---------------------------------------
+# -----------------------------------
 
 plt.figure(figsize=(12,4))
 
@@ -76,33 +76,4 @@ plt.xlabel("Pixel Intensity")
 plt.ylabel("Frequency")
 
 plt.tight_layout()
-plt.show()
-
-
-contrast = cv2.equalizeHist(gray)
-
-plt.figure(figsize=(10,4))
-
-plt.subplot(1,2,1)
-plt.imshow(gray, cmap='gray')
-plt.title("Before Enhancement")
-plt.axis('off')
-
-plt.subplot(1,2,2)
-plt.imshow(contrast, cmap='gray')
-plt.title("After Histogram Equalization")
-plt.axis('off')
-
-plt.show()
-
-plt.figure(figsize=(10,4))
-
-plt.subplot(1,2,1)
-plt.hist(gray.ravel(), bins=256, range=[0,256])
-plt.title("Original Histogram")
-
-plt.subplot(1,2,2)
-plt.hist(contrast.ravel(), bins=256, range=[0,256])
-plt.title("Enhanced Histogram")
-
 plt.show()
